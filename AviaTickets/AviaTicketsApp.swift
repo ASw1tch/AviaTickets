8f8e08cf84b5149f78485853723c1b4ae6b89584
Anatolii Petrov <hello@aswitch.me>
2024-05-29 20:48:39 +0200

    Initial Commit

---
+++ AviaTickets/AviaTicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviaTicketsApp.swift
+//  AviaTickets
+//
+//  Created by Anatoliy Petrov on 29.5.24..
+//
+
+import SwiftUI
+
+@main
+struct AviaTicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
